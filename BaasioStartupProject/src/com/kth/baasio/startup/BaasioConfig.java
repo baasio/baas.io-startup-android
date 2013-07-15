897b17a7253ca21b8fd488a045deac8dffd9ab82
Brad <brad.hong@kt.com>
2013-07-15 15:25:26 +0900

    BAAS-315

--- BaasioStartupProject/src/com/kth/baasio/startup/BaasioConfig.java
+++ BaasioStartupProject/src/com/kth/baasio/startup/BaasioConfig.java
@@ -5,12 +5,12 @@ public class BaasioConfig {
     public static final String BAASIO_URL = "https://api.baas.io";
 
     // baas.io member ID
-    public static final String BAASIO_ID = "PUT_YOUR_MEMBER_ID";
+    public static final String BAASIO_ID = "PUT_YOUR_BAASIO_MEMBER_ID";
 
     // baas.io application ID
-    public static final String APPLICATION_ID = "sandbox";
+    public static final String APPLICATION_ID = "PUT_YOUR_SANDBOX_APPLICATION_ID";
 
     // GCM(google cloud messaging service) sender ID
-    public static final String GCM_SENDER_ID = null;	
+    public static final String GCM_SENDER_ID = null;
 
 }
